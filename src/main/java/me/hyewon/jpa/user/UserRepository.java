69e87ea110c89fb58076ebd28c593a46e3941852
hyewon218 <won_11111@naver.com>
2023-07-31 20:42:00 +0900

    feat(2day) : 정렬(Sort, JpaSort)

--- src/main/java/me/hyewon/jpa/user/UserRepository.java
+++ src/main/java/me/hyewon/jpa/user/UserRepository.java
@@ -1,8 +1,19 @@
 package me.hyewon.jpa.user;
 
-import me.hyewon.jpa.my.MyRepository;
+import java.util.List;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface UserRepository extends JpaRepository<User, Long>, MyRepository<User> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
+  // @Query 사용시 Alias(쿼리에서 as 로 지정한 문구) 를 기준으로 정렬할 수 있다.
+  // 아래와 같이 AS user_password 로 Alias(AS) 를 걸어주면
+  @Query("SELECT u, u.password AS customField FROM User u WHERE u.username = ?1")
+  List<User> findByUsernameWithCustomField(String username, Sort sort);
+
+  // JpaSort 를 사용해서 쿼리 함수를 기준으로 정렬할 수 있다.
+  // 아래와 같이 일반적인 쿼리에서
+  @Query("SELECT u FROM User u WHERE u.username = ?1")
+  List<User> findByUsername(String username, Sort sort);
 }
--- src/test/java/me/hyewon/jpa/userchannel/UserChannelRepositoryTest.java
+++ src/test/java/me/hyewon/jpa/userchannel/UserChannelRepositoryTest.java
@@ -7,6 +7,8 @@ import me.hyewon.jpa.user.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.JpaSort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,49 +17,83 @@ import org.springframework.transaction.annotation.Transactional;
 @Rollback(value = false)
 class UserChannelRepositoryTest {
 
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired
+  private UserRepository userRepository;
 
-    @Autowired
-    private ChannelRepository channelRepository;
+  @Autowired
+  private ChannelRepository channelRepository;
 
-    @Test
-    void userJoinChannelTest() {
-        // given
-        var newChannel = Channel.builder().name("new-channel").build();
-        var newUser = User.builder().username("new_user").password("new-pass").build();
-        var newUserChannel = newChannel.joinUser(newUser);
+  @Test
+  void userJoinChannelTest() {
+    // given
+    var newChannel = Channel.builder().name("new-channel").build();
+    var newUser = User.builder().username("new_user").password("new-pass").build();
+    var newUserChannel = newChannel.joinUser(newUser);
 
-        // when
-        // 각각의 entity 저장(영속성 상태)
-        var savedChannel = channelRepository.insertChannel(newChannel);
-        var savedUser = userRepository.insertUser(newUser);
+    // when
+    // 각각의 entity 저장(영속성 상태)
+    var savedChannel = channelRepository.save(newChannel);
+    var savedUser = userRepository.save(newUser);
 
-        // then
-        var foundChannel = channelRepository.selectChannel(savedChannel.getId());
-        // 저장한 테이블의 UserChannels 에 우리가 설정한 채널의 이름과 같은 것이 있는지 확인
-        assert foundChannel.getUserChannels().stream()
-                .map(UserChannel::getChannel)
-                .map(Channel::getName)
-                .anyMatch(name -> name.equals(newChannel.getName()));
-    }
+    // then
+    var foundChannel = channelRepository.findById(savedChannel.getId());
+    // 저장한 테이블의 UserChannels 에 우리가 설정한 채널의 이름과 같은 것이 있는지 확인
+    assert foundChannel.get().getUserChannels().stream()
+        .map(UserChannel::getChannel)
+        .map(Channel::getName)
+        .anyMatch(name -> name.equals(newChannel.getName()));
+  }
 
-    @Test
-    void userJoinChannelWithCascadeTest() {
-        // given
-        var newChannel = Channel.builder().name("new-channel").build();
-        var newUser = User.builder().username("new_user").password("new-pass").build();
-        newChannel.joinUser(newUser); // userChannels cascade = CascadeType.ALL -> joinUser 만 하더라도 실제 저장이 된다!
+  @Test
+  void userJoinChannelWithCascadeTest() {
+    // given
+    var newChannel = Channel.builder().name("new-channel").build();
+    var newUser = User.builder().username("new_user").password("new-pass").build();
+    newChannel.joinUser(
+        newUser); // userChannels cascade = CascadeType.ALL -> joinUser 만 하더라도 실제 저장이 된다!
 
-        // when
-        var savedChannel = channelRepository.insertChannel(newChannel);
-        var savedUser = userRepository.insertUser(newUser);
+    // when
+    var savedChannel = channelRepository.save(newChannel);
+    var savedUser = userRepository.save(newUser);
 
-        // then
-        var foundChannel = channelRepository.selectChannel(savedChannel.getId());
-        assert foundChannel.getUserChannels().stream()
-                .map(UserChannel::getChannel)
-                .map(Channel::getName)
-                .anyMatch(name -> name.equals(newChannel.getName()));
-    }
+    // then
+    var foundChannel = channelRepository.findById(savedChannel.getId());
+    assert foundChannel.get().getUserChannels().stream()
+        .map(UserChannel::getChannel)
+        .map(Channel::getName)
+        .anyMatch(name -> name.equals(newChannel.getName()));
+  }
+
+  @Test
+  void userCustomFieldSortingTest() {
+    // given
+    var newUser1 = User.builder().username("new_user").password("new-pass1").build();
+    var newUser2 = User.builder().username("new_user").password("new-pass2").build();
+    userRepository.save(newUser1);
+    userRepository.save(newUser2);
+
+    // when
+    var users = userRepository.findByUsernameWithCustomField("new_user", Sort.by("customField"));
+
+    // then
+    assert users.get(0).getPassword().equals(newUser1.getPassword());
+
+    // when
+    users = userRepository.findByUsernameWithCustomField("new_user",
+        Sort.by("customField").descending());
+
+    // then
+    assert users.get(0).getPassword().equals(newUser2.getPassword());
+
+    // JpaSort 를 사용해서 쿼리 함수를 기준으로 정렬
+    var newUser3 = User.builder().username("new_user").password("3").build();
+    userRepository.save(newUser3);
+
+    // when
+    users = userRepository.findByUsername("new_user",
+        JpaSort.unsafe("LENGTH(password)"));
+
+    // then
+    assert users.get(0).getPassword().equals(newUser3.getPassword());
+  }
 }

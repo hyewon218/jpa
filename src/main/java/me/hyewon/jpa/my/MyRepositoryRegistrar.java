ae58a91eaabd0bdf4e099c864fb8b582ba3b60ac
hyewon218 <won_11111@naver.com>
2023-07-31 12:35:50 +0900

    feat(2day) : 프로그래밍을 통한 레파지토리 주입

---
+++ src/main/java/me/hyewon/jpa/my/MyRepositoryRegistrar.java
@@ -0,0 +1,24 @@
+package me.hyewon.jpa.my;
+
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.type.AnnotationMetadata;
+
+import java.util.Map;
+
+public class MyRepositoryRegistrar implements ImportBeanDefinitionRegistrar {
+
+    @Override
+    public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata,
+                                        BeanDefinitionRegistry registry) {
+
+        // 주입할 빈에 대해 프로그래밍 하는 부분!!
+        GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+        beanDefinition.setBeanClass(MyRepository.class);
+        beanDefinition.getPropertyValues().add("dataTable", Map.of(1L, "data"));
+        // 여기까지!
+
+        registry.registerBeanDefinition("myRepository", beanDefinition);
+    }
+}
---
+++ src/test/java/me/hyewon/jpa/my/MyRepositoryRegistrarTest.java
@@ -0,0 +1,27 @@
+package me.hyewon.jpa.my;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(MyRepositoryRegistrar.class) // 빈 주입!
+@SpringBootTest
+public class MyRepositoryRegistrarTest {
+
+    @Autowired
+    MyRepository myRepository;
+
+    @Test
+    void myRepositoryTest() {
+        // given
+        var newData = "NEW DATA";
+        var savedId = myRepository.save(newData);
+
+        // when
+        var newDataList = myRepository.find(savedId);
+
+        // then
+        System.out.println(newDataList);
+    }
+}
